2fa5759dc42013574dc1dc79958534747a5fe3f2
lajbel <lajbelms@gmail.com>
2024-05-27 10:31:24 -0300

    style: fmt constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -1,32 +1,32 @@
 // some default charsets for loading bitmap fonts
 export const ASCII_CHARS =
-  " !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~"
-export const DEF_ANCHOR = "topleft"
-export const BG_GRID_SIZE = 64
-export const DEF_FONT = "monospace"
-export const DBG_FONT = "monospace"
-export const DEF_TEXT_SIZE = 36
-export const DEF_TEXT_CACHE_SIZE = 64
-export const MAX_TEXT_CACHE_SIZE = 256
-export const FONT_ATLAS_WIDTH = 2048
-export const FONT_ATLAS_HEIGHT = 2048
-export const SPRITE_ATLAS_WIDTH = 2048
-export const SPRITE_ATLAS_HEIGHT = 2048
+    " !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~";
+export const DEF_ANCHOR = "topleft";
+export const BG_GRID_SIZE = 64;
+export const DEF_FONT = "monospace";
+export const DBG_FONT = "monospace";
+export const DEF_TEXT_SIZE = 36;
+export const DEF_TEXT_CACHE_SIZE = 64;
+export const MAX_TEXT_CACHE_SIZE = 256;
+export const FONT_ATLAS_WIDTH = 2048;
+export const FONT_ATLAS_HEIGHT = 2048;
+export const SPRITE_ATLAS_WIDTH = 2048;
+export const SPRITE_ATLAS_HEIGHT = 2048;
 // 0.1 pixel padding to texture coordinates to prevent artifact
-export const UV_PAD = 0.1
-export const DEF_HASH_GRID_SIZE = 64
-export const DEF_FONT_FILTER = "linear"
-export const LOG_MAX = 8
-export const LOG_TIME = 4
+export const UV_PAD = 0.1;
+export const DEF_HASH_GRID_SIZE = 64;
+export const DEF_FONT_FILTER = "linear";
+export const LOG_MAX = 8;
+export const LOG_TIME = 4;
 export const VERTEX_FORMAT = [
-	{ name: "a_pos", size: 2 },
-	{ name: "a_uv", size: 2 },
-	{ name: "a_color", size: 4 },
-]
-const STRIDE = VERTEX_FORMAT.reduce((sum, f) => sum + f.size, 0)
-const MAX_BATCHED_QUAD = 2048
-export const MAX_BATCHED_VERTS = MAX_BATCHED_QUAD * 4 * STRIDE
-export const MAX_BATCHED_INDICES = MAX_BATCHED_QUAD * 6
+    { name: "a_pos", size: 2 },
+    { name: "a_uv", size: 2 },
+    { name: "a_color", size: 4 },
+];
+const STRIDE = VERTEX_FORMAT.reduce((sum, f) => sum + f.size, 0);
+const MAX_BATCHED_QUAD = 2048;
+export const MAX_BATCHED_VERTS = MAX_BATCHED_QUAD * 4 * STRIDE;
+export const MAX_BATCHED_INDICES = MAX_BATCHED_QUAD * 6;
 // vertex shader template, replace {{user}} with user vertex shader code
 export const VERT_TEMPLATE = `
 attribute vec2 a_pos;
@@ -50,7 +50,7 @@ void main() {
 	v_color = a_color;
 	gl_Position = pos;
 }
-`
+`;
 // fragment shader template, replace {{user}} with user fragment shader code
 export const FRAG_TEMPLATE = `
 precision mediump float;
@@ -73,32 +73,32 @@ void main() {
 		discard;
 	}
 }
-`
+`;
 // default {{user}} vertex shader code
 export const DEF_VERT = `
 vec4 vert(vec2 pos, vec2 uv, vec4 color) {
 	return def_vert();
 }
-`
+`;
 // default {{user}} fragment shader code
 export const DEF_FRAG = `
 vec4 frag(vec2 pos, vec2 uv, vec4 color, sampler2D tex) {
 	return def_frag();
 }
-`
-export const COMP_DESC = new Set(["id", "require"])
+`;
+export const COMP_DESC = new Set(["id", "require"]);
 export const COMP_EVENTS = new Set([
-	"add",
-	"update",
-	"draw",
-	"destroy",
-	"inspect",
-	"drawInspect",
-])
+    "add",
+    "update",
+    "draw",
+    "destroy",
+    "inspect",
+    "drawInspect",
+]);
 // TODO: escape
 // eslint-disable-next-line
-export const TEXT_STYLE_RE = /\[(?<style>\w+)\](?<text>.*?)\[\/\k<style>\]/g
-export const DEF_OFFSCREEN_DIS = 200
+export const TEXT_STYLE_RE = /\[(?<style>\w+)\](?<text>.*?)\[\/\k<style>\]/g;
+export const DEF_OFFSCREEN_DIS = 200;
 // maximum y velocity with body()
-export const DEF_JUMP_FORCE = 640
-export const MAX_VEL = 65536
+export const DEF_JUMP_FORCE = 640;
+export const MAX_VEL = 65536;
