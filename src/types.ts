e20b49e082ea5f2a4158a4f413ddb716658406fd
lajbel <lajbelms@gmail.com>
2024-05-30 08:18:14 -0300

    doc: fix readd example

--- src/types.ts
+++ src/types.ts
@@ -145,34 +145,34 @@ export interface KaboomCtx {
      *
      * // Create two sprites.
      * const greenBean = add([
-     * sprite("bean"),
-     * pos(200,140),
-     * color(255, 255, 255),
-     * area(),
-     * ])
+     *     sprite("bean"),
+     *     pos(200,140),
+     *     color(255, 255, 255),
+     *     area(),
+     * ]);
      *
      * // This bean will overlap the green bean.
      * const purpleBean = add([
-     * sprite("bean"),
-     * pos(230,140),
-     * color(255, 0, 255),
-     * area(),
-     * ])
+     *     sprite("bean"),
+     *     pos(230,140),
+     *     color(255, 0, 255),
+     *     area(),
+     * ]);
      *
      * // Example 1: simply call readd() on the target you want on top.
-     * readd(greenBean)
+     * readd(greenBean);
      *
      * // Example 2: using onClick() or other functions with readd().
      * // If you comment out the first example, and use this readd() with a function like onClick(), you
      * can keep switching which sprite is above the other ( click on edge of face ).
      *
      * purpleBean.onClick(() => {
-     * 		readd(greenBean)
-     * })
+     *     readd(greenBean)
+     * });
      *
      * greenBean.onClick(() => {
-     * 		readd(purpleBean)
-     * })
+     *     readd(purpleBean)
+     * });
      * ```
      *
      * @group Game Obj
