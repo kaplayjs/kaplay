e37b46e9383ab9fbdd7e21eca2caa229ee1572a0
SpectCOW <126259962+SpcFORK@users.noreply.github.com>
2024-06-04 01:13:42 -0400

    fix: Avoid TS error..?

--- src/kaboom.ts
+++ src/kaboom.ts
@@ -3264,7 +3264,7 @@ const kaplay = (gopt: KaboomOpt = {}): KaboomCtx => {
                 }
 
                 // class object
-                if (isClass(comp)) comp = new comp;
+                if (isClass(comp)) (comp as any) = new comp;
 
                 // tag
                 if (typeof comp === "string") {
