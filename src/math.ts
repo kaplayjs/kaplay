c488184c8ee501ee41e0c63ab0707e6a2cbb934d
lajbel <lajbelms@gmail.com>
2024-07-07 14:45:22 +0000

    fix: type rand and randi

--- src/math.ts
+++ src/math.ts
@@ -1318,13 +1318,12 @@ export function randSeed(seed?: number): number {
     return defRNG.seed;
 }
 
-export function rand(...args) {
-    // @ts-ignore
+export function rand<T = number>(...args: [] | [T] | [T, T]) {
     return defRNG.genAny(...args);
 }
 
 // TODO: randi() to return 0 / 1?
-export function randi(...args: number[]) {
+export function randi(...args: [] | [number] | [number, number]) {
     return Math.floor(rand(...args));
 }
 
