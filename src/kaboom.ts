017d8e4d8efb22eac09eb12ea732588b8ceeb475
SpectCOW <126259962+SpcFORK@users.noreply.github.com>
2024-07-07 14:05:39 -0400

    fix: type game `logs` (#222)

--- src/kaboom.ts
+++ src/kaboom.ts
@@ -750,7 +750,7 @@ const kaplay = <
         defaultLayerIndex: 0,
 
         // on screen log
-        logs: [],
+        logs: [] as { msg: string | { toString(): string; }, time: number, }[],
 
         // camera
         cam: {
