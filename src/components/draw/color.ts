05830727af82b32527a2768528a90ba6d5c585ad
SpectCOW <126259962+SpcFORK@users.noreply.github.com>
2024-07-07 13:40:05 -0400

    fix: Implement expected overflows for Color (#231)

--- src/components/draw/color.ts
+++ src/components/draw/color.ts
@@ -1,4 +1,4 @@
-import { type Color, rgb } from "../../math";
+import { type Color, rgb } from "../../math/color";
 import type { Comp } from "../../types";
 
 /**
@@ -10,7 +10,16 @@ export interface ColorComp extends Comp {
     color: Color;
 }
 
-export function color(...args): ColorComp {
+type ARGcase = [number, number, number];
+
+type Colorcase = Color
+type RBGcase = ARGcase
+type STRcase = string
+type NONEcase = void
+
+type ColorArgs = [Colorcase | RBGcase | STRcase | NONEcase] | ARGcase;
+
+export function color(...args: ColorArgs): ColorComp {
     return {
         id: "color",
         color: rgb(...args),
