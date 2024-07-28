5e0bd4095d491f69733521e089599ff5229e841f
Marc Flerackers <mflerackers@gmail.com>
2024-07-28 10:47:20 +0900

    fix: fixedDt (#287)

--- src/app/frame.ts
+++ src/app/frame.ts
@@ -3,3 +3,7 @@ import { app, debug } from "../kaplay";
 export function dt() {
     return app.dt() * debug.timeScale;
 }
+
+export function fixedDt() {
+    return app.fixedDt() * debug.timeScale;
+}
