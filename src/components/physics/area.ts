38019b18cda18fb219be74dc0c5dccd76ec36d52
Daniel Báez <danielbaezibarrola+githubsus@gmail.com>
2024-08-30 17:36:07 -0300

    feat: fake mouse (#373)

--- src/components/physics/area.ts
+++ src/components/physics/area.ts
@@ -2,8 +2,7 @@ import { DEF_ANCHOR } from "../../constants";
 import { isFixed } from "../../game/utils";
 import { anchorPt, getViewportScale } from "../../gfx";
 import { app, game, k } from "../../kaplay";
-import { rgb } from "../../math/color";
-import { Polygon, testPolygonPoint, Vec2, vec2 } from "../../math/math";
+import { Polygon, rgb, testPolygonPoint, Vec2, vec2 } from "../../math";
 import type {
     Collision,
     Comp,
@@ -14,9 +13,8 @@ import type {
     Tag,
 } from "../../types";
 import type { KEventController } from "../../utils/";
-import type { AnchorComp } from "../transform/anchor";
-import type { FixedComp } from "../transform/fixed";
-import type { PosComp } from "../transform/pos";
+import type { FakeMouseComp } from "../misc";
+import type { AnchorComp, FixedComp, PosComp } from "../transform";
 
 let areaCount = 0;
 
@@ -214,9 +212,17 @@ export interface AreaCompOpt {
     collisionIgnore?: Tag[];
 }
 
+let fakeMouse: GameObj<FakeMouseComp | PosComp> | null = null;
+let fakeMouseChecked = false;
+
 export function area(opt: AreaCompOpt = {}): AreaComp {
     const colliding: Record<string, Collision> = {};
     const collidingThisFrame = new Set();
+
+    if (!fakeMouse && !fakeMouseChecked) {
+        fakeMouse = k.get<FakeMouseComp | PosComp>("fakeMouse")[0];
+        fakeMouseChecked = true;
+    }
 
     return {
         id: "area",
@@ -308,13 +314,23 @@ export function area(opt: AreaCompOpt = {}): AreaComp {
         },
 
         isClicked(): boolean {
+            if (fakeMouse) {
+                return fakeMouse.isPressed && this.isHovering();
+            }
+
             return app.isMousePressed() && this.isHovering();
         },
 
-        isHovering(this: GameObj) {
-            const mpos = isFixed(this)
-                ? k.mousePos()
-                : k.toWorld(k.mousePos());
+        isHovering(this: GameObj<AreaComp>) {
+            if (fakeMouse) {
+                const mpos = isFixed(this)
+                    ? fakeMouse.pos
+                    : k.toWorld(fakeMouse.pos);
+
+                return this.hasPoint(mpos);
+            }
+
+            const mpos = isFixed(this) ? k.mousePos() : k.toWorld(k.mousePos());
             return this.hasPoint(mpos);
         },
 
@@ -353,14 +369,23 @@ export function area(opt: AreaCompOpt = {}): AreaComp {
 
         onClick(
             this: GameObj<AreaComp>,
-            f: () => void,
+            action: () => void,
             btn: MouseButton = "left",
         ): KEventController {
-            const e = app.onMousePress(btn, () => {
+            if (fakeMouse) {
+                fakeMouse.onPress(() => {
+                    if (this.isHovering()) {
+                        action();
+                    }
+                });
+            }
+
+            const e = k.onMousePress(btn, () => {
                 if (this.isHovering()) {
-                    f();
+                    action();
                 }
             });
+
             this.onDestroy(() => e.cancel());
             return e;
         },
