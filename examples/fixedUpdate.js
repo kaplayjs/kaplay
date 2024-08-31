ba00ee741b4183cefcca2638ba373d3ca419ac39
Marc Flerackers <mflerackers@gmail.com>
2024-08-31 21:44:30 +0900

    feat: Makes onFixedUpdate attach to fixedUpdate (#374)

---
+++ examples/fixedUpdate.js
@@ -0,0 +1,17 @@
+kaplay();
+
+let fixedCount = 0;
+let count = 0;
+
+onFixedUpdate(() => {
+    fixedCount++;
+});
+
+onUpdate(() => {
+    count++;
+    debug.log(
+        `${fixedDt()} ${Math.floor(fixedCount / time())} ${dt()} ${
+            Math.floor(count / time())
+        }`,
+    );
+});
