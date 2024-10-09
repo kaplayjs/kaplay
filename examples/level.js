dbba287b49070b97c85d150083e42a5a80127cce
Arthur Martins Braga <arthur.martinsbraga@telecom-paris.fr>
2024-10-09 16:40:36 +0200

    chore: reverting level.js changes

--- examples/level.js
+++ examples/level.js
@@ -1,30 +1,87 @@
+// @ts-check
+
+// Build levels with addLevel()
+
+// Start game
 kaplay();
 
+// Load assets
+loadSprite("bean", "/sprites/bean.png");
+loadSprite("coin", "/sprites/coin.png");
+loadSprite("spike", "/sprites/spike.png");
 loadSprite("grass", "/sprites/grass.png");
+loadSprite("ghosty", "/sprites/ghosty.png");
+loadSound("score", "/examples/sounds/score.mp3");
+
+const SPEED = 480;
+
+setGravity(2400);
 
 const level = addLevel([
-    " =",
+    // Design the level layout with symbols
+    "@  ^ $$",
+    "=======",
 ], {
+    // The size of each grid
     tileWidth: 64,
     tileHeight: 64,
+    // The position of the top left block
+    pos: vec2(100, 200),
+    // Define what each symbol means (in components)
     tiles: {
+        "@": () => [
+            sprite("bean"),
+            area(),
+            body(),
+            anchor("bot"),
+            "player",
+        ],
         "=": () => [
             sprite("grass"),
+            area(),
+            body({ isStatic: true }),
+            anchor("bot"),
+        ],
+        "$": () => [
+            sprite("coin"),
+            area(),
+            anchor("bot"),
+            "coin",
+        ],
+        "^": () => [
+            sprite("spike"),
+            area(),
+            anchor("bot"),
+            "danger",
         ],
     },
 });
 
-const pos = vec2(0, 0);
-level.spawn("=", pos);
+// Get the player object from tag
+const player = level.get("player")[0];
 
-wait(1, () => {
-    const obj = level.getAt(pos);
-    console.log(obj);
-    destroy(obj[0]);
+// Movements
+onKeyPress("space", () => {
+    if (player.isGrounded()) {
+        player.jump();
+    }
 });
 
-wait(2, () => {
-    // shouldn't print the object
-    const obj = level.getAt(pos);
-    console.log(obj);
+onKeyDown("left", () => {
+    player.move(-SPEED, 0);
+});
+
+onKeyDown("right", () => {
+    player.move(SPEED, 0);
+});
+
+// Back to the original position if hit a "danger" item
+player.onCollide("danger", () => {
+    player.pos = level.tile2Pos(0, 0);
+});
+
+// Eat the coin!
+player.onCollide("coin", (coin) => {
+    destroy(coin);
+    play("score");
 });
