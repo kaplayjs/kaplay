c4506f7e14074444d30c5adfb452b43fef679d80
lajbel <lajbelms@gmail.com>
2024-10-31 20:59:01 -0300

    doc(examples): update bench.js to have different rendering scenes

--- examples/bench.js
+++ examples/bench.js
@@ -7,22 +7,80 @@ kaplay();
 
 loadSprite("bean", "sprites/bean.png");
 loadSprite("bag", "sprites/bag.png");
+loadSprite("bobo", "sprites/bobo.png");
+loadSprite("ghosty", "sprites/ghosty.png");
 
-// Adds 5 thousand objects which can be a bean or a bag in random positions
-for (let i = 0; i < 5000; i++) {
-    add([
-        sprite(i % 2 === 0 ? "bean" : "bag"),
-        pos(rand(0, width()), rand(0, height())),
-        anchor("center"),
-    ]).paused = true;
+const scenes = [
+    "sprites",
+    "objects",
+];
+
+scene("sprites", () => {
+    debug.log("Rendering 5000 sprites using onDraw");
+
+    // Adds 5 thousand sprites which can be a bean or a bag in random positions
+    const sprites = [];
+
+    for (let i = 0; i < 5000; i++) {
+        sprites.push({
+            sprite: i % 2 === 0 ? "bean" : "bag",
+            pos: vec2(rand(0, width()), rand(0, height())),
+            anchor: "center",
+        });
+    }
+
+    onDraw(() => {
+        sprites.forEach((sprite) => {
+            drawSprite(sprite);
+        });
+    });
+
+    addFps();
+    addSceneNavigation();
+});
+
+scene("objects", () => {
+    debug.log("Rendering 5000 sprites using game objects");
+
+    // Adds 5 thousand objects which can be a bean or a bag in random positions
+    for (let i = 0; i < 5000; i++) {
+        add([
+            sprite(i % 2 === 0 ? "bobo" : "ghosty"),
+            pos(rand(0, width()), rand(0, height())),
+            anchor("center"),
+        ]).paused = true;
+    }
+
+    addFps();
+    addSceneNavigation();
+});
+
+go("sprites");
+
+function addFps() {
+    onDraw(() => {
+        drawText({
+            // You can get the current fps with debug.fps()
+            text: debug.fps(),
+            pos: vec2(width() / 2, height() / 2),
+            anchor: "center",
+            color: rgb(255, 127, 255),
+            outline: {
+                width: 10,
+            },
+        });
+    });
 }
 
-onDraw(() => {
-    drawText({
-        // You can get the current fps with debug.fps()
-        text: debug.fps(),
-        pos: vec2(width() / 2, height() / 2),
-        anchor: "center",
-        color: rgb(255, 127, 255),
+function addSceneNavigation() {
+    onKeyPress("1", () => go("sprites"));
+    onKeyPress("2", () => go("objects"));
+
+    onTouchStart(() => {
+        const currentScene = getSceneName();
+        const nextScene =
+            scenes[(scenes.indexOf(currentScene) + 1) % scenes.length];
+
+        go(nextScene);
     });
-});
+}
