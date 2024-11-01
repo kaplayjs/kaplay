41aadf866ae9c375b024d7a446e5a0385513bbfc
lajbel <lajbelms@gmail.com>
2024-10-31 21:26:54 -0300

    chore: add uvquad

--- examples/bench.js
+++ examples/bench.js
@@ -13,6 +13,7 @@ loadSprite("ghosty", "sprites/ghosty.png");
 const scenes = [
     "sprites",
     "objects",
+    "uvquads",
 ];
 
 scene("sprites", () => {
@@ -55,6 +56,37 @@ scene("objects", () => {
     addSceneNavigation();
 });
 
+scene("uvquads", () => {
+    debug.log("Rendering 5000 uv quads using onDraw");
+
+    const quads = [];
+    const bean = getSprite("bean");
+    const bag = getSprite("bag");
+    // We only need the bean texture since the bag texture is on the same atlas
+    const tex = bean.data.tex;
+    const beanQuad = bean.data.frames[0];
+    const bagsQuad = bag.data.frames[0];
+
+    for (let i = 0; i < 5000; i++) {
+        quads.push({
+            tex: tex,
+            quad: i % 2 === 0 ? beanQuad : bagsQuad,
+            pos: vec2(rand(0, width()), rand(0, height())),
+            width: i % 2 === 0 ? bean.data.width : bag.data.width,
+            height: i % 2 === 0 ? bean.data.height : bag.data.height,
+        });
+    }
+
+    onDraw(() => {
+        quads.forEach((quad) => {
+            drawUVQuad(quad);
+        });
+    });
+
+    addFps();
+    addSceneNavigation();
+});
+
 go("sprites");
 
 function addFps() {
@@ -75,6 +107,7 @@ function addFps() {
 function addSceneNavigation() {
     onKeyPress("1", () => go("sprites"));
     onKeyPress("2", () => go("objects"));
+    onKeyPress("3", () => go("uvquads"));
 
     onTouchStart(() => {
         const currentScene = getSceneName();
