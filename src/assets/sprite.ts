59ddc461e3d3c852558ba5db5fece8a5370568be
Francisco Barreiras <126603030+chqs-git@users.noreply.github.com>
2025-01-15 15:24:53 +0000

    feat: added option for sprites to be imported as a separate texture (#581)

--- src/assets/sprite.ts
+++ src/assets/sprite.ts
@@ -67,6 +67,10 @@ export interface LoadSpriteOpt {
      * Animation configuration.
      */
     anims?: SpriteAnims;
+    /**
+     * If the sprite is a single image.
+     */
+    singular?: boolean;
 }
 
 export type NineSlice = {
@@ -132,7 +136,9 @@ export class SpriteData {
         data: ImageSource,
         opt: LoadSpriteOpt = {},
     ): SpriteData {
-        const [tex, quad] = _k.assets.packer.add(data);
+        const [tex, quad] = opt.singular
+            ? _k.assets.packer.add_single(data)
+            : _k.assets.packer.add(data);
         const frames = opt.frames
             ? opt.frames.map((f) =>
                 new Quad(
