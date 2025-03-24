4c9ee05784bf0836d6b5f50d8fc6eb51c723f759
Daniel Báez <danielbaezibarrola+githubsus@gmail.com>
2025-03-23 23:17:01 -0300

    test: new testing (#656)

---
+++ tests/auto/kaplay.spec.ts
@@ -0,0 +1,18 @@
+import { beforeAll, describe, expect, it } from "vitest";
+import "vitest-puppeteer";
+import "kaplay/global";
+
+describe("KAPLAY Context Initialization", () => {
+    beforeAll(async () => {
+        await page.addScriptTag({ path: "dist/kaplay.js" });
+    });
+
+    it("VERSION should be defined", async () => {
+        const version = await page.evaluate(() => {
+            kaplay();
+            return VERSION;
+        });
+
+        expect(version).toBeDefined();
+    }, 20000);
+});
---
+++ tests/auto/missingComps.spec.ts
@@ -0,0 +1,30 @@
+import { beforeAll, describe, expect, it } from "vitest";
+import "vitest-puppeteer";
+import "kaplay/global";
+
+describe("Missing Comps", async () => {
+    beforeAll(async () => {
+        await page.addScriptTag({ path: "dist/kaplay.js" });
+    });
+
+    it("Use body() without pos() in add() should throw an error", async () => {
+        async function useBodyWithoutPos() {
+            return page.evaluate(() => {
+                kaplay();
+
+                return new Promise((res, rej) => {
+                    onError((e) => {
+                        console.log(e);
+                        rej(e.message);
+                    });
+
+                    add([
+                        body(),
+                    ]);
+                });
+            });
+        }
+
+        await expect(useBodyWithoutPos).rejects.toThrow(/requires/);
+    }, 20000);
+});
