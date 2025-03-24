2a71fa2eb66a8e9505c8ccf7ed2bf4e123f4d9fa
lajbel <lajbelms@gmail.com>
2025-03-23 22:37:59 -0300

    test: new testing using vite-puppeteer

---
+++ vitest.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+    test: {
+        name: "KAPLAY.js",
+        environment: "puppeteer",
+        globalSetup: "vitest-environment-puppeteer/global-init",
+        fileParallelism: false,
+    },
+});
