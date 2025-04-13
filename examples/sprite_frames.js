31b0a8932b09cf5e0d63c6a8319080f934cd57e0
nojaf <florian.verdonck@outlook.com>
2025-04-13 11:01:23 +0200

    Format

--- examples/sprite_frames.js
+++ examples/sprite_frames.js
@@ -8,10 +8,10 @@
 
 // Start a kaboom game
 kaplay({
-  // Scale the whole game up
-  scale: 4,
-  // Set the default font
-  font: "monospace",
+    // Scale the whole game up
+    scale: 4,
+    // Set the default font
+    font: "monospace",
 });
 
 // We store the original width and height of loaded sprite file.
@@ -20,72 +20,72 @@ const originalHeight = 512;
 
 // Loading predefined sprite frames
 loadSprite("dino", "/sprites/dungeon.png", {
-  frames: [
-    // Every input of a quad is between 0 and 1.
-    // So, we need do some calculations to get the right quad.
-    // Note that these frames were measured by hand, this is only for demonstration.
-    quad(
-      129 / originalWidth,
-      234 / originalHeight,
-      15 / originalWidth,
-      22 / originalHeight
-    ),
-    quad(
-      145 / originalWidth,
-      234 / originalHeight,
-      15 / originalWidth,
-      22 / originalHeight
-    ),
-    quad(
-      161 / originalWidth,
-      234 / originalHeight,
-      15 / originalWidth,
-      22 / originalHeight
-    ),
-    quad(
-      177 / originalWidth,
-      234 / originalHeight,
-      15 / originalWidth,
-      22 / originalHeight
-    ),
-    quad(
-      193 / originalWidth,
-      234 / originalHeight,
-      15 / originalWidth,
-      22 / originalHeight
-    ),
-    quad(
-      209 / originalWidth,
-      234 / originalHeight,
-      15 / originalWidth,
-      22 / originalHeight
-    ),
-    quad(
-      225 / originalWidth,
-      234 / originalHeight,
-      15 / originalWidth,
-      22 / originalHeight
-    ),
-    quad(
-      241 / originalWidth,
-      234 / originalHeight,
-      15 / originalWidth,
-      22 / originalHeight
-    ),
-    quad(
-      257 / originalWidth,
-      234 / originalHeight,
-      15 / originalWidth,
-      22 / originalHeight
-    ),
-  ],
-  anims: {
-    run: {
-      from: 0,
-      to: 8,
-      loop: true,
+    frames: [
+        // Every input of a quad is between 0 and 1.
+        // So, we need do some calculations to get the right quad.
+        // Note that these frames were measured by hand, this is only for demonstration.
+        quad(
+            129 / originalWidth,
+            234 / originalHeight,
+            15 / originalWidth,
+            22 / originalHeight,
+        ),
+        quad(
+            145 / originalWidth,
+            234 / originalHeight,
+            15 / originalWidth,
+            22 / originalHeight,
+        ),
+        quad(
+            161 / originalWidth,
+            234 / originalHeight,
+            15 / originalWidth,
+            22 / originalHeight,
+        ),
+        quad(
+            177 / originalWidth,
+            234 / originalHeight,
+            15 / originalWidth,
+            22 / originalHeight,
+        ),
+        quad(
+            193 / originalWidth,
+            234 / originalHeight,
+            15 / originalWidth,
+            22 / originalHeight,
+        ),
+        quad(
+            209 / originalWidth,
+            234 / originalHeight,
+            15 / originalWidth,
+            22 / originalHeight,
+        ),
+        quad(
+            225 / originalWidth,
+            234 / originalHeight,
+            15 / originalWidth,
+            22 / originalHeight,
+        ),
+        quad(
+            241 / originalWidth,
+            234 / originalHeight,
+            15 / originalWidth,
+            22 / originalHeight,
+        ),
+        quad(
+            257 / originalWidth,
+            234 / originalHeight,
+            15 / originalWidth,
+            22 / originalHeight,
+        ),
+    ],
+    anims: {
+        run: {
+            from: 0,
+            to: 8,
+            loop: true,
+        },
     },
-  },
 });
 
 // Add our player character
