663fe1937cbe439eb9b370c6d8a5aecb0ac49047
Daniel Báez <danielbaezibarrola@gmail.com>
2025-05-14 07:37:26 -0300

    fix: TexPacker.add wasn't returning in add_single, add_single -> addSingle (v3001) (#778)

--- src/gfx/texPacker.ts
+++ src/gfx/texPacker.ts
@@ -35,7 +35,7 @@ export class TexPacker {
     }
 
     // create a image with a single texture
-    add_single(img: ImageSource): [Texture, Quad, number] {
+    addSingle(img: ImageSource): [Texture, Quad, number] {
         const tex = Texture.fromImage(this.gfx, img);
         this.bigTextures.push(tex);
         return [tex, new Quad(0, 0, 1, 1), 0];
@@ -48,7 +48,7 @@ export class TexPacker {
         if (
             paddedWidth > this.canvas.width || paddedHeight > this.canvas.height
         ) {
-            this.add_single(img);
+            return this.addSingle(img);
         }
 
         // next row
