adab032081ce2dd1dd7a280749738d138ad233a6
Marc Flerackers <mflerackers@gmail.com>
2025-06-30 18:58:21 +0900

    move serialization

--- src/ecs/components/transform/move.ts
+++ src/ecs/components/transform/move.ts
@@ -2,7 +2,7 @@ import { Vec2 } from "../../../math/Vec2";
 import type { EmptyComp, GameObj } from "../../../types";
 import type { PosComp } from "./pos";
 
-export function move(dir: number | Vec2, speed: number): EmptyComp {
+export function move(dir: number | Vec2, speed: number): EmptyComp & { serialize: () => any } {
     const d = typeof dir === "number" ? Vec2.fromAngle(dir) : dir.unit();
     return {
         id: "move",
@@ -10,5 +10,12 @@ export function move(dir: number | Vec2, speed: number): EmptyComp {
         update(this: GameObj<PosComp>) {
             this.move(d.scale(speed));
         },
+        serialize() {
+            return { dir: dir instanceof Vec2 ? { x: dir.x, y: dir.y } : dir, speed: speed }
+        }
     };
+}
+
+export function moveFactory(data: any) {
+    return move(data.dir, data.speed);
 }
