3e4c5c5143ddad17d95e0be807c4e3e352718ec9
amyspark-ng <92493175+amyspark-ng@users.noreply.github.com>
2025-07-05 13:07:53 -0500

    clear input events when doing clearEvents() in an obj

--- src/ecs/entity/GameObjRaw.ts
+++ src/ecs/entity/GameObjRaw.ts
@@ -1363,6 +1363,10 @@ export const GameObjRawPrototype: Omit<InternalGameObjRaw, AppEvents> = {
         this._drawEvents.clear();
         this._updateEvents.clear();
         this._fixedUpdateEvents.clear();
+        for (let i = this._inputEvents.length - 1; i >= 0; i--) {
+            this._inputEvents[i].cancel()
+            this._inputEvents.pop()
+        }
     },
     // #endregion
 
