397fa19f53207365df382f6ca07f4f918d733908
dragoncoder047 <101021094+dragoncoder047@users.noreply.github.com>
2025-08-26 11:39:28 -0400

    formatter

--- src/ecs/components/draw/text.ts
+++ src/ecs/components/draw/text.ts
@@ -251,8 +251,8 @@ export function text(t: string, opt: TextCompOpt = {}): TextComp {
                 lineSpacing: this.lineSpacing,
                 letterSpacing: this.letterSpacing,
                 indentAll: opt.indentAll,
-            }
-        }
+            };
+        },
     };
 
     // @ts-expect-error
