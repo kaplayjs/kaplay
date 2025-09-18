b69c21747d002366127adfe67f8b20e413e30e79
dragoncoder047 <101021094+dragoncoder047@users.noreply.github.com>
2025-09-18 09:41:56 -0400

    formatter

--- tests/playtests/textItalic.js
+++ tests/playtests/textItalic.js
@@ -2,25 +2,28 @@ kaplay({ background: "black" });
 
 add([
     pos(100, 100),
-    text(`this is [b]bold[/b] and this is [i]italicized[/i] woo!
+    text(
+        `this is [b]bold[/b] and this is [i]italicized[/i] woo!
 [b][i]have some of both![/i][/b]
-[o]and some oblique text[/o]`, {
-        styles: {
-            b: {
-                color: WHITE,
-                override: true,
+[o]and some oblique text[/o]`,
+        {
+            styles: {
+                b: {
+                    color: WHITE,
+                    override: true,
+                },
+                i: {
+                    skew: 20,
+                },
+                o: {
+                    color: RED,
+                    skew: -20,
+                    override: true,
+                },
             },
-            i: {
-                skew: 20,
+            transform: {
+                color: rgb("gray"),
             },
-            o: {
-                color: RED,
-                skew: -20,
-                override: true,
-            }
         },
-        transform: {
-            color: rgb("gray"),
-        },
-    }),
+    ),
 ]);
