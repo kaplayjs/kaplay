810485cb12f740e26c870a3d9b43ff9dcd247527
KiwiSky <neonixbuilds@gmail.com>
2025-09-21 20:33:23 -0300

    comp: updates do scrollcomp

--- src/ecs/components/transform/scroll.ts
+++ src/ecs/components/transform/scroll.ts
@@ -1,34 +1,50 @@
 import { getCamPos } from "../../../game/camera";
-import { vec2, type Vec2Args } from "../../../math/math";
-import type { Vec2 } from "../../../math/Vec2";
+import { vec2 } from "../../../math/math";
+import { Vec2 } from "../../../math/Vec2";
 import type { Comp, GameObj } from "../../../types";
+import type { PosComp } from "./pos";
 
-export interface ScrollFactorComp extends Comp {
-    pos: Vec2;
+export interface CamScrollComp extends Comp {
     factor: Vec2;
 
-    set(x: number, y: number): void;
+    setScrollFactor(x: number, y: number): void;
+    getScrollFactor(): Vec2;
 }
 
-export function scroll(...args: Vec2Args) {
+export function camscroll(x: number = 0, y: number = 1): CamScrollComp {
     return {
         id: "scroll",
         require: ["pos"],
-        factor: vec2(...args),
+        factor: vec2(x, y),
 
-        update(this: GameObj) {
-            let cameraPos: Vec2 = getCamPos();
-            let objPos: Vec2 = this.worldPos();
+        update() {
+            //let cameraPos: Vec2 = getCamPos();
+            //let objPos: any = this.worldPos();
 
-            this.pos.x = objPos.x - cameraPos.x * this.factor.x;
-            this.pos.y = objPos.y - cameraPos.y * this.factor.y;
+            //this.pos.x = objPos.x - cameraPos.x * this.factor.x;
+            //this.pos.y = objPos.y - cameraPos.y * this.factor.y;
+
+            const self = this as unknown as GameObj<PosComp>;
+            const cam = getCamPos();
+
+            const objPos = self.worldPos();
+            if (objPos)
+            {
+                self.pos.x = objPos.x - cam.x * this.factor.x;
+                self.pos.y = objPos.y - cam.y * this.factor.y;
+            }
         },
 
-        set(x: number, y: number) {
+        setScrollFactor(x: number, y: number) {
+            this.factor.x, this.factor.y = x, y;
+        },
+
+        getScrollFactor(){
+            return this.factor.clone();
         },
 
         inspect() {
             return `scrollFactor: vec2(${this.factor.x}, ${this.factor.x})`;
-        },
+        }
     };
 }
