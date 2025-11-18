231b23f07e5af855ad46d1466228a553b6b26e3d
Anthony G <good.anthony@gmail.com>
2025-11-18 14:51:41 +0000

    fix: Render Narrow/Tall Fonts Without Cropping Them (#950)

---
+++ tests/playtests/textTall.js
@@ -0,0 +1,43 @@
+/**
+ * @file Tall fonts test
+ * @description Test the rendering of tall fonts to ensure they're not cropped.
+ * @difficulty 99
+ * @tags basics
+ * @minver 4000.0
+ */
+
+kaplay({ background: "#000000", crisp: true });
+const nabla = loadFont(
+    "Nabla",
+    "/fonts/Nabla-Regular-VariableFont_EDPT,EHLT.ttf",
+    { filter: "nearest" },
+);
+const anton = loadFont("Anton", "/fonts/Anton-Regular.ttf", {
+    filter: "nearest",
+});
+
+const obj = add([
+    pos(10, 10),
+    text(
+        "This is a tall font\nThat shouldn't \nget cropped by KAPLAY",
+        {
+            font: "Nabla",
+            width: width(),
+            size: 48,
+            color: rgb(255, 255, 255),
+        },
+    ),
+]);
+
+add([
+    pos(10, 250),
+    text(
+        "This is another tall font\nThat also \nshouldn't get cropped",
+        {
+            font: "Anton",
+            width: width(),
+            size: 32,
+            color: rgb(255, 255, 0),
+        },
+    ),
+]);
