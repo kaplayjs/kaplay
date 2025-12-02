4466df99c9720d63b883bfae7a3988fdcfa7161d
dragoncoder047 <101021094+dragoncoder047@users.noreply.github.com>
2025-12-02 12:16:39 -0500

    feat: make Vec2 able to do math on noninstances

--- src/math/math.ts
+++ src/math/math.ts
@@ -11,7 +11,7 @@ import { clamp } from "./clamp";
 import { Color, rgb } from "./color";
 import { traceRegion } from "./getImageOutline";
 import { lerp, type LerpValue } from "./lerp";
-import { Vec2 } from "./Vec2";
+import { Vec2, type SerializedVec2 } from "./Vec2";
 
 /**
  * Possible arguments for a Vec2.
@@ -20,11 +20,10 @@ import { Vec2 } from "./Vec2";
  * @subgroup Vectors
  */
 export type Vec2Args =
-    | [number, number]
-    | [number]
-    | [Vec2]
-    | [number | Vec2]
-    | [];
+    | [number, number] // x = args[0]   y = args[1]
+    | [number]         // x = args[0]   y = x
+    | [SerializedVec2] // x = args[0].x y = args[0].y
+    | [];              // x = 0         y = 0
 
 export function deg2rad(deg: number): number {
     return deg * Math.PI / 180;
@@ -65,7 +64,7 @@ export function smoothstep(edge0: number, edge1: number, x: number) {
 
 export function vec2(...args: Vec2Args): Vec2 {
     if (args.length === 1) {
-        if (args[0] instanceof Vec2) {
+        if (typeof args[0] === "object") {
             return new Vec2(args[0].x, args[0].y);
         }
         else if (Array.isArray(args[0]) && args[0].length === 2) {
