5b075ffb1b9675b72d4beabf7aa64a86aa25da4b
amyspark-ng <92493175+amyspark-ng@users.noreply.github.com>
2026-01-04 16:56:35 -0500

    added sensors, more levels, win screen

--- examples/sokoban.js
+++ examples/sokoban.js
@@ -9,29 +9,60 @@
 
 kaplay({
     background: [45, 33, 51],
+    buttons: {
+        "left": { keyboard: ["left", "a"] },
+        "down": { keyboard: ["down", "s"] },
+        "up": { keyboard: ["up", "w"] },
+        "right": { keyboard: ["right", "d"] },
+        "undo": { keyboard: "z" },
+    },
 });
 
 loadSprite("bean", "/sprites/bean.png");
 loadSprite("grass", "/sprites/grass.png");
 loadSprite("steel", "/sprites/steel.png");
 
-const level = addLevel([
+let lvlIdx = 0;
+let canMove = true;
+let lastState = null;
+
+const levels = [[
     ".......",
-    ".p   d.",
+    ".ps sd.",
     ". b b .",
     ".     .",
     ".......",
-], {
+], [
+    "........",
+    ". p.   .",
+    ".  b   .",
+    ".  .  ..",
+    ".s .  .",
+    ".......",
+], [
+    " ......",
+    "..s   ..",
+    ".  .b  .",
+    ".  bs  .",
+    ". p   ..",
+    "..  ... ",
+    " ....   ",
+]];
+
+const tiles = {
+    "p": () => [sprite("bean"), z(1), "player"],
+    "b": () => [sprite("grass"), z(1), "box"],
+    ".": () => [sprite("steel"), "wall"],
+    "s": () => [rect(64, 64), color(), "sensor"],
+};
+
+let level = addLevel(levels[lvlIdx], {
     tileWidth: 64,
     tileHeight: 64,
-    tiles: {
-        p: () => [sprite("bean"), "player"],
-        b: () => [sprite("grass"), "box"],
-        ".": () => [sprite("steel"), "wall"],
-    },
+    tiles: tiles,
 });
 
-const player = level.get("player")[0];
+let player = level.get("player")[0];
 
 const hasTag = (objs, tag) => objs.findIndex(obj => obj.is(tag)) !== -1;
 
@@ -44,38 +75,69 @@ const moveObj = (obj, dir) => {
 
 const move = (dir) => {
     const moveTo = player.tilePos.add(dir);
-    const occupant = level.getAt(moveTo);
+    // the object that exists at the position the player is going to move to
+    const occupant = level.getAt(moveTo).filter((obj) => !obj.is("sensor"));
 
-    if (hasTag(occupant, "wall")) {
-        return;
-    }
+    if (hasTag(occupant, "wall")) return;
 
     if (hasTag(occupant, "box")) {
         const boxMoveTo = occupant[0].tilePos.add(dir);
-        const boxOccupant = level.getAt(boxMoveTo);
+        // if there's an object at the position the box will get moved to
+        const boxOccupant = level.getAt(boxMoveTo).filter((obj) =>
+            !obj.is("sensor")
+        );
 
-        if (boxOccupant.length !== 0) {
-            return;
+        if (boxOccupant.length == 0) {
+            moveObj(occupant[0], dir);
         }
-
-        moveObj(occupant[0], dir);
+        else return;
     }
 
     moveObj(player, dir);
+
+    // time to serialize for the undo
+    level.children.forEach((child, idx) => {
+    });
+
+    // if every sensor has a box in the same tile pos trigger win
+    const allSensors = level.children.filter((c) => c.is("sensor"));
+    if (
+        allSensors.every((sensor) =>
+            level.getAt(sensor.tilePos).some((obj) => obj.is("box"))
+        )
+    ) {
+        win();
+    }
 };
 
-onKeyPress("d", () => {
-    move(vec2(1, 0));
-});
+const win = () => {
+    canMove = false;
 
-onKeyPress("a", () => {
-    move(vec2(-1, 0));
-});
+    level.destroy();
+    level = null;
 
-onKeyPress("w", () => {
-    move(vec2(0, -1));
-});
+    if (lvlIdx == levels.length - 1) {
+        add([
+            text("YOU WON!!"),
+        ]);
+        return;
+    }
 
-onKeyPress("s", () => {
-    move(vec2(0, 1));
+    lvlIdx++;
+    level = addLevel(levels[lvlIdx], {
+        tileWidth: 64,
+        tileHeight: 64,
+        tiles: tiles,
+    });
+    player = level.get("player")[0];
+    canMove = true;
+};
+
+onButtonPress((button) => {
+    if (!canMove) return;
+
+    if (button == "right") move(vec2(1, 0));
+    else if (button == "left") move(vec2(-1, 0));
+    else if (button == "up") move(vec2(0, -1));
+    else if (button == "down") move(vec2(0, 1));
 });
