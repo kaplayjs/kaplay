f7279a427178945a36208464192e4576f63f9443
amyspark-ng <92493175+amyspark-ng@users.noreply.github.com>
2026-01-06 00:11:07 -0500

    small polish, new sprites

--- examples/sokoban.js
+++ examples/sokoban.js
@@ -19,12 +19,9 @@ kaplay({
 });
 
 loadSprite("bean", "/sprites/bean.png");
-loadSprite("grass", "/sprites/grass.png");
+loadSprite("box", "/sprites/box.png");
+loadSprite("sturdybox", "/sprites/sturdybox.png");
 loadSprite("steel", "/sprites/steel.png");
-
-let lvlIdx = 0;
-let canMove = true;
-let undoStack = []; // { dir, box }
 
 const levels = [[
     ".......",
@@ -50,21 +47,26 @@ const levels = [[
 ]];
 
 const tiles = {
-    "p": () => [sprite("bean"), z(1), "player"],
-    "b": () => [sprite("grass"), z(1), "box"],
-    ".": () => [sprite("steel"), "wall"],
-    "s": () => [rect(64, 64), color(), "sensor"],
+    "p": () => [sprite("bean"), z(1), scale(), anchor("center"), "player"],
+    "b": () => [sprite("box"), z(1), scale(), anchor("center"), "box"],
+    ".": () => [sprite("steel"), scale(), anchor("center"), "wall"],
+    "s": () => [rect(64, 64), color(), scale(), anchor("center"), "sensor"],
 };
 
-let level = addLevel(levels[lvlIdx], {
-    tileWidth: 64,
-    tileHeight: 64,
-    tiles: tiles,
-});
+let moves = 0;
+let undos = 0;
 
-let player = level.get("player")[0];
+let currentIdx = 0;
+let boxesInSensors = 0;
+let canMove = true;
+let undoStack = []; // { dir, box }
+
+let level;
+let player;
 
 const hasTag = (objs, tag) => objs.findIndex(obj => obj.is(tag)) !== -1;
+
+const isBoxInSensor = (box) => hasTag(level.getAt(box.tilePos), "sensor");
 
 const moveObj = (obj, dir) => {
     if (dir.x == 1) obj.moveRight();
@@ -81,86 +83,229 @@ const move = (dir) => {
         !obj.is("sensor")
     );
 
-    if (hasTag(occupant, "wall")) return;
+    if (hasTag(occupant, "wall")) {
+        tween(
+            vec2(1.2),
+            vec2(1),
+            0.12,
+            (p) => occupant[0].scale = p,
+            easings.easeOutQuad,
+        );
+
+        return;
+    }
 
     let box = null;
 
     if (hasTag(occupant, "box")) {
         box = occupant[0];
+
+        // check if box is sturdy (already on sensor)
+        if (
+            level.get("sensor").some((sensor) =>
+                level.getAt(sensor.tilePos).includes(box)
+            )
+        ) {
+            tween(
+                vec2(1.2),
+                vec2(1),
+                0.12,
+                (p) => occupant[0].scale = p,
+                easings.easeOutQuad,
+            );
+            return;
+        }
+
         const checkAtPos = occupant[0].tilePos.add(dir);
 
         // if there's an object at the position the box will get moved to
-        const boxOccupant = level.getAt(checkAtPos).filter((obj) =>
-            !obj.is("sensor")
-        );
+        const boxOccupants = level.getAt(checkAtPos);
 
-        if (boxOccupant.length == 0) {
+        if (
+            !boxOccupants[0]
+            || boxOccupants[0].is("sensor") && !hasTag(boxOccupants, "box")
+        ) {
             moveObj(box, dir);
+            tween(
+                vec2(1.2),
+                vec2(1),
+                0.12,
+                (p) => box.scale = p,
+                easings.easeOutQuad,
+            );
+
+            if (boxOccupants[0]?.is("sensor")) {
+                boxesInSensors++;
+                tween(
+                    vec2(1.2),
+                    vec2(1),
+                    0.12,
+                    (p) => box.scale = p,
+                    easings.easeOutQuad,
+                );
+            }
         }
-        else return;
+        else {
+            tween(
+                vec2(1.2),
+                vec2(1),
+                0.12,
+                (p) => player.scale = p,
+                easings.easeOutQuad,
+            );
+
+            tween(
+                vec2(1.2),
+                vec2(1),
+                0.12,
+                (p) => box.scale = p,
+                easings.easeOutQuad,
+            );
+            return;
+        }
+
+        if (isBoxInSensor(box)) box.sprite = "sturdybox";
+        else box.sprite = "box";
     }
 
+    moves++;
     moveObj(player, dir);
+    tween(
+        vec2(1.2),
+        vec2(1),
+        0.12,
+        (p) => player.scale = p,
+        easings.easeOutQuad,
+    );
 
     undoStack.push({
         dir: dir,
         box: occupant[0],
     });
 
-    console.log(undoStack);
+    boxesInSensors =
+        level.get("sensor").map((sensor) =>
+            hasTag(level.getAt(sensor.tilePos), "box")
+        ).filter((el) => el == true).length;
 
     // if every sensor has a box in the same tile pos trigger win
-    const allSensors = level.children.filter((c) => c.is("sensor"));
-    if (
-        allSensors.every((sensor) =>
-            level.getAt(sensor.tilePos).some((obj) => obj.is("box"))
-        )
-    ) {
-        win();
+    if (boxesInSensors == level.get("sensor").length) {
+        currentIdx++;
+        canMove = false;
+        level.children.forEach((child, idx) => {
+            tween(
+                child.scale,
+                vec2(0),
+                0.05 * idx,
+                (p) => child.scale = p,
+                easings.easeInBack,
+            );
+        });
+        wait(0.05 * level.children.length, () => {
+            if (currentIdx > levels.length - 1) {
+                go("win");
+            }
+            else go("game", currentIdx);
+        });
     }
 };
 
-const win = () => {
-    canMove = false;
-
-    level.destroy();
-    level = null;
+scene("game", (lvlIdx) => {
+    moves = 0;
+    undos = 0;
     undoStack = [];
 
-    if (lvlIdx == levels.length - 1) {
-        add([
-            text("YOU WON!!"),
-        ]);
-        return;
-    }
-
-    lvlIdx++;
     level = addLevel(levels[lvlIdx], {
         tileWidth: 64,
         tileHeight: 64,
         tiles: tiles,
     });
+
+    const levelSize = vec2(
+        level.tileWidth() * level.numRows(),
+        level.tileHeight() * level.numColumns(),
+    );
+    level.pos = center().sub(levelSize.scale(0.5));
     player = level.get("player")[0];
-    canMove = true;
-};
 
-onButtonPress((button) => {
-    if (!canMove) return;
+    level.children.forEach((child, idx) => {
+        child.scale = vec2(0);
+        tween(
+            child.scale,
+            vec2(1),
+            0.07 * idx,
+            (p) => child.scale = p,
+            easings.easeOutBack,
+        ).onEnd(() => {
+            canMove = true;
+        });
+    });
 
-    if (button == "left") move(vec2(-1, 0));
-    else if (button == "right") move(vec2(1, 0));
-    else if (button == "up") move(vec2(0, -1));
-    else if (button == "down") move(vec2(0, 1));
+    onButtonPress((button) => {
+        if (!canMove) return;
+
+        if (button == "left") move(vec2(-1, 0));
+        else if (button == "right") move(vec2(1, 0));
+        else if (button == "up") move(vec2(0, -1));
+        else if (button == "down") move(vec2(0, 1));
+    });
+
+    onButtonPress("undo", () => {
+        if (undoStack.length === 0) return;
+
+        undos++;
+        const move = undoStack.pop();
+
+        // move player back
+        moveObj(player, move.dir.scale(-1));
+        tween(
+            vec2(0.8),
+            vec2(1),
+            0.15,
+            (p) => player.scale = p,
+            easings.easeOutQuad,
+        );
+
+        // move box back if there was one
+        if (move.box) {
+            moveObj(move.box, move.dir.scale(-1));
+            tween(
+                vec2(0.8),
+                vec2(1),
+                0.15,
+                (p) => move.box.scale = p,
+                easings.easeOutQuad,
+            );
+            if (isBoxInSensor(move.box)) move.box.sprite = "sturdybox";
+            else move.box.sprite = "box";
+        }
+    });
 });
 
-onButtonPress("undo", () => {
-    if (undoStack.length === 0) return;
+scene("win", () => {
+    add([
+        anchor("center"),
+        pos(center().sub(0, 150)),
+        text("Bravo!\n[small]You succesfully restocked soks[/small]", {
+            size: 50,
+            align: "center",
+            styles: {
+                "small": {
+                    scale: vec2(0.8),
+                },
+            },
+        }),
+    ]);
 
-    const move = undoStack.pop();
+    add([
+        sprite("box"),
+        scale(3),
+        pos(center().add(0, 150)),
+        anchor("center"),
+    ]);
 
-    // move player back
-    moveObj(player, move.dir.scale(-1));
-
-    // move box back if there was one
-    if (move.box) moveObj(move.box, move.dir.scale(-1));
+    // add particle of soks coming out of the box
 });
+
+go("game", currentIdx);
+go("win");
