c3f30e5aea8c436a95221260822525e31601c3c5
dragoncoder047 <101021094+dragoncoder047@users.noreply.github.com>
2026-02-06 22:47:07 -0500

    whoopsie

--- src/gfx/draw/drawSprite.ts
+++ src/gfx/draw/drawSprite.ts
@@ -64,7 +64,7 @@ export function drawSprite(opt: DrawSpriteOpt) {
     }
 
     // TODO: slow
-    var spriteData: SpriteData;
+    let spriteData: SpriteData;
 
     if (!(opt.sprite instanceof SpriteData)) {
         const spr = resolveSprite(opt.sprite);
