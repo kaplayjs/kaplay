1f7189982b49d348f7cb2348c79dde1e033834eb
lajbel <71136486+lajbel@users.noreply.github.com>
2026-06-16 21:42:34 -0300

    test: remove firefox from testing

--- playwright.config.ts
+++ playwright.config.ts
@@ -32,10 +32,5 @@ export default defineConfig({
             name: "chromium",
             use: { ...devices["Desktop Chrome"] },
         },
-
-        {
-            name: "firefox",
-            use: { ...devices["Desktop Firefox"] },
-        },
     ],
 });
