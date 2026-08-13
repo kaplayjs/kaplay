699973221516a7cd3c91aea134f86bf0a0467a32
imaginarny <gencurpeter@gmail.com>
2026-08-13 23:12:50 +0200

    chore: playtests

--- tests/playtests/mouseLetterbox.js
+++ tests/playtests/mouseLetterbox.js
@@ -1,7 +1,10 @@
 kaplay({
-    width: 400,
-    height: 200,
+    width: 640,
+    height: 360,
     letterbox: true,
+    crisp: true,
+    logMax: 1,
+    logTime: Infinity,
 });
 
 const redDot = add([
@@ -9,6 +12,7 @@ const redDot = add([
     circle(3),
     color(RED),
     pos(),
+    area(),
 ]);
 
 onMouseMove(pos => {
@@ -18,3 +22,44 @@ onMouseMove(pos => {
 onKeyPress("f", () => {
     setFullscreen(!isFullscreen());
 });
+
+onDraw(() => {
+    drawRect({
+        width: width(),
+        height: height(),
+        fill: false,
+        outline: {
+            width: 4,
+        },
+    });
+});
+
+const logInfo = () =>
+    debug.log([
+        ``,
+        `Setup:    ${_k.globalOpt.width ?? "100%"}x${
+            _k.globalOpt.height ?? "100%"
+        } @${_k.globalOpt.pixelDensity ?? 1} x${_k.globalOpt.scale ?? 1}`,
+        `Canvas:   ${_k.canvas.offsetWidth}x${_k.canvas.offsetHeight}`,
+        `Buffer:   ${
+            `${_k.gfx.gl.drawingBufferWidth}x${_k.gfx.gl.drawingBufferHeight}`
+                .padEnd(9, " ")
+        } (Size@Density${
+            _k.globalOpt.lockResolution
+                || (_k.globalOpt.width && !_k.globalOpt.letterbox)
+                ? "*Scale"
+                : ""
+        })`,
+        `Gfx:      ${
+            `${Math.round(_k.gfx.width)}x${Math.round(_k.gfx.height)}`
+                .padEnd(9, " ")
+        } (Buffer/Density/Scale)`,
+        `Viewport: ${Math.round(_k.gfx.screenViewport.width)}x${
+            Math.round(_k.gfx.screenViewport.height)
+        } gfx@${Math.round(_k.gfx.screenViewport.scale * 100) / 100} ${
+            Math.round(_k.gfx.screenViewport.x)
+        }x ${Math.round(_k.gfx.screenViewport.y)}y`,
+    ].join("\n"));
+
+logInfo();
+onResize(logInfo);
--- tests/playtests/mouseSizeFixed.js
+++ tests/playtests/mouseSizeFixed.js
@@ -2,7 +2,12 @@
 // then press and hold mouse down and move around
 // ensure that red dot follows correctly in fullscreen (F)
 
-kaplay({ width: 600, height: 600 });
+kaplay({
+    width: 800,
+    height: 600,
+    logMax: 1,
+    logTime: Infinity,
+});
 
 const redDot = add([
     anchor("center"),
@@ -19,3 +24,44 @@ onMouseMove(pos => {
 onKeyPress("f", () => {
     setFullscreen(!isFullscreen());
 });
+
+onDraw(() => {
+    drawRect({
+        width: width(),
+        height: height(),
+        fill: false,
+        outline: {
+            width: 4,
+        },
+    });
+});
+
+const logInfo = () =>
+    debug.log([
+        ``,
+        `Setup:    ${_k.globalOpt.width ?? "100%"}x${
+            _k.globalOpt.height ?? "100%"
+        } @${_k.globalOpt.pixelDensity ?? 1} x${_k.globalOpt.scale ?? 1}`,
+        `Canvas:   ${_k.canvas.offsetWidth}x${_k.canvas.offsetHeight}`,
+        `Buffer:   ${
+            `${_k.gfx.gl.drawingBufferWidth}x${_k.gfx.gl.drawingBufferHeight}`
+                .padEnd(9, " ")
+        } (Size@Density${
+            _k.globalOpt.lockResolution
+                || (_k.globalOpt.width && !_k.globalOpt.letterbox)
+                ? "*Scale"
+                : ""
+        })`,
+        `Gfx:      ${
+            `${Math.round(_k.gfx.width)}x${Math.round(_k.gfx.height)}`
+                .padEnd(9, " ")
+        } (Buffer/Density/Scale)`,
+        `Viewport: ${Math.round(_k.gfx.screenViewport.width)}x${
+            Math.round(_k.gfx.screenViewport.height)
+        } gfx@${Math.round(_k.gfx.screenViewport.scale * 100) / 100} ${
+            Math.round(_k.gfx.screenViewport.x)
+        }x ${Math.round(_k.gfx.screenViewport.y)}y`,
+    ].join("\n"));
+
+logInfo();
+onResize(logInfo);
