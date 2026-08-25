458281493894fd5a38fc529dd08e241959e1fbff
lajbel <71136486+lajbel@users.noreply.github.com>
2026-08-25 00:45:33 +0000

    style: fmt

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,27 +12,26 @@ import { defineConfig, devices } from "@playwright/test";
  * see https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-	testDir: "./tests/",
-	testIgnore: ["**/tests/.archive/**"],
-	/* run tests in files in parallel */
-	fullyParallel: true,
-	/* reporter to use. see https://playwright.dev/docs/test-reporters */
-	reporter: "html",
-	/* shared settings for all the projects below. see https://playwright.dev/docs/api/class-testoptions. */
-	use: {
-		/* base url to use in actions like `await page.goto('')`. */
-		// baseurl: 'http://localhost:3000',
+    testDir: "./tests/",
+    testIgnore: ["**/tests/.archive/**"],
+    /* run tests in files in parallel */
+    fullyParallel: true,
+    /* reporter to use. see https://playwright.dev/docs/test-reporters */
+    reporter: "html",
+    /* shared settings for all the projects below. see https://playwright.dev/docs/api/class-testoptions. */
+    use: {
+        /* base url to use in actions like `await page.goto('')`. */
+        // baseurl: 'http://localhost:3000',
 
-		/* collect trace when retrying the failed test. see https://playwright.dev/docs/trace-viewer */
-		trace: "on-first-retry",
-	},
+        /* collect trace when retrying the failed test. see https://playwright.dev/docs/trace-viewer */
+        trace: "on-first-retry",
+    },
 
-	/* configure projects for major browsers */
-	projects: [
-		{
-			name: "chromium",
-			use: { ...devices["desktop chrome"] },
-		},
-	],
+    /* configure projects for major browsers */
+    projects: [
+        {
+            name: "chromium",
+            use: { ...devices["desktop chrome"] },
+        },
+    ],
 });
-
