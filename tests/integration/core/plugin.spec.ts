4e0f41c1938c9c4483167d5c0f457c8d23ba4b8f
lajbel <71136486+lajbel@users.noreply.github.com>
2026-08-31 18:53:15 -0300

    test: from pupeteer/vitest to playwright + new testing organization (#1156)

---
+++ tests/integration/core/plugin.spec.ts
@@ -0,0 +1,41 @@
+import { expect, test } from "@playwright/test";
+import type { KAPLAYCtx } from "../../../src";
+
+test.beforeEach(async ({ page }) => {
+    await page.addScriptTag({ path: "dist/kaplay.js" });
+});
+
+test("plugin methods should exist in context", async ({ page }) => {
+    page.on("console", msg => console.log(`Browser log: ${msg.text()}`));
+
+    const method = await page.evaluate(() => {
+        const testPlugin = (k: KAPLAYCtx) => ({
+            myMethod() {
+                return k.VERSION;
+            },
+        });
+
+        const k = kaplay({ plugins: [testPlugin] });
+
+        // Tests only admits serializable valuies
+        return k.myMethod !== undefined && k.myMethod !== null;
+    });
+
+    expect(method).toBeTruthy();
+});
+
+test("plugin methods should work in context", async ({ page }) => {
+    const [version, methodResult] = await page.evaluate(() => {
+        const testPlugin = (k: KAPLAYCtx) => ({
+            myMethod() {
+                return k.VERSION;
+            },
+        });
+
+        const k = kaplay({ plugins: [testPlugin] });
+
+        return [k.VERSION, k.myMethod()];
+    });
+
+    expect(methodResult).toBe(version);
+});
